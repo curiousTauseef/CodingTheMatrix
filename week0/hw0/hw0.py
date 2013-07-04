2c24202ba0ac656804cb6a03f539bd3c08f1013a
Michael Recachinas <mgr3yp@virginia.edu>
2013-07-03 22:33:04 -0400

    Finished hw0

--- week0/hw0/hw0.py
+++ week0/hw0/hw0.py
@@ -1,30 +1,21 @@
 # Please fill out this stencil and submit using the provided submission script.
 
-
-
-
-
 ## Problem 1
 def myFilter(L, num): return [ x for x in L if x%num != 0 ]
 
-
-
 ## Problem 2
-def myLists(L): return [ [x,y] for y in L for x in range(1,y+1) ]
-
-
+def myLists(L): return [ list[range(1,x+1)] for x in L ]
 
 ## Problem 3
-def myFunctionComposition(f, g): return {x:y for x in f.keys() for y in g.values() if f.values() == g.keys() }
-
+def myFunctionComposition(f, g): return { k:g[f[k]] for x in f.keys() for k in f.keys() }
 
 ## Problem 4
 # Please only enter your numerical solution.
 
-complex_addition_a = complex(5,3) 
-complex_addition_b = complex(0,1)
-complex_addition_c = complex(-1,.001)
-complex_addition_d = complex(.001,9)
+complex_addition_a = complex(5,3) 		# 5+3j
+complex_addition_b = complex(0,1)		# 1j
+complex_addition_c = complex(-1,.001)	# -1+0.001j
+complex_addition_d = complex(.001,9)	# 0.001+9j
 
 ## Problem 5
 GF2_sum_1 = 1
